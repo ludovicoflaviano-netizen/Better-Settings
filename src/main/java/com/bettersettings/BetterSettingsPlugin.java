5dfab4192e480091f0efb397cb6bef6a9244abf2
ludovicoflaviano-netizen <ludovico.flaviano@gmail.com>
2026-09-13 14:30:05 +0700

    Fix dialog action import for Paper 1.21.11 build

--- src/main/java/com/bettersettings/BetterSettingsPlugin.java
+++ src/main/java/com/bettersettings/BetterSettingsPlugin.java
@@ -3,6 +3,7 @@ package com.bettersettings;
 import io.papermc.paper.dialog.Dialog;
 import io.papermc.paper.registry.data.dialog.*;
 import io.papermc.paper.registry.data.dialog.body.DialogBody;
+import io.papermc.paper.registry.data.dialog.action.DialogAction;
 import io.papermc.paper.registry.data.dialog.input.DialogInput;
 import io.papermc.paper.registry.data.dialog.type.DialogType;
 import net.kyori.adventure.text.Component;
